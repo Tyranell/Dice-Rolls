5fb7cf317df581e27fcb5907b234c6e26c8dde73
Damian Jordan <damianjordan83@gmail.com>
2024-01-19 15:13:52 +0100

    Addition and subtraction button split
    Eureka! The buttons now actually get printed! button-container is now split in add-button-container and sub-button-container and subtraction buttons got added to begin with.

--- logic.js
+++ logic.js
@@ -2,11 +2,19 @@
 const numberOfAdditionButtons = 20;
 const numberOfSubtractionButtons = 20;
 
-const buttonContainer = document.getElementById("button-container");
+const addButtonContainer = document.getElementById("add-button-container");
+const subButtonContainer = document.getElementById("sub-button-container");
 
 for (let i = 1; i <= numberOfAdditionButtons; i++) {
   const addButton = document.createElement("button");
   addButton.textContent = "Add 1";
   addButton.setAttribute("id", `buttonA${i}`);
-  buttonContainer.appendChild(addButton);
+  addButtonContainer.appendChild(addButton);
+}
+
+for (let i = 1; i <= numberOfSubtractionButtons; i++) {
+  const subButton = document.createElement("button");
+  subButton.textContent = "Subtract 1";
+  subButton.setAttribute("id", `buttonS${i}`);
+  subButtonContainer.appendChild(subButton);
 }
