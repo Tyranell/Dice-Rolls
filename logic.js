83d65ff3713d503044ffdd75665db8cb9266c3eb
Damian Jordan <damianjordan83@gmail.com>
2024-01-19 20:53:18 +0100

    Subtraction buttons and numbers stacked
    This now should stack the other two properly as well

--- logic.js
+++ logic.js
@@ -11,6 +11,7 @@ for (let i = 1; i <= numberOfAdditionButtons; i++) {
   const addButton = document.createElement("button");
   addButton.textContent = "Add 1";
   addButton.setAttribute("id", `buttonA${i}`);
+  addButton.style.gridArea = `aB${i}`;
   addButtonContainer.appendChild(addButton);
 }
 
@@ -18,6 +19,7 @@ for (let i = 1; i <= numberOfSubtractionButtons; i++) {
   const subButton = document.createElement("button");
   subButton.textContent = "Subtract 1";
   subButton.setAttribute("id", `buttonS${i}`);
+  addButton.style.gridArea = `sB${i}`;
   subButtonContainer.appendChild(subButton);
 }
 
@@ -25,5 +27,6 @@ for (let i = 1; i <= numberOfNumbers; i++) {
   const numberFields = document.createElement("div");
   numberFields.textContent = i;
   numberFields.setAttribute("id", `number${i}`);
+  addButton.style.gridArea = `nu${i}`;
   numberContainer.appendChild(numberFields);
 }
