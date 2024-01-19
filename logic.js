8c09d133a99fb0276327e03e2fcc41d33f2fce42
Damian Jordan <damianjordan83@gmail.com>
2024-01-19 14:52:10 +0100

    Buttons hopefully
    Added code into logic.js that hopefully adds the addition buttons. No code that supports anything from them yet, just the buttons themselves.

--- logic.js
+++ logic.js
@@ -1,1 +1,12 @@
 //Spooky! The start of a JS file
+const numberOfAdditionButtons = 20;
+const numberOfSubtractionButtons = 20;
+
+const buttonContainer = document.getElementById("button-container");
+
+for (let i = 1; i <= numberOfAdditionButtons; i++) {
+  const addButton = document.createElement("button");
+  button.textContent = "Add 1";
+  button.setAttribute("id", "buttonA{i}");
+  buttonContainer.appendChild(button);
+}
