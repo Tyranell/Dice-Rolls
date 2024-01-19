13e97b87ed3b7260b373b49e5d12139acaf7e671
Damian Jordan <damianjordan83@gmail.com>
2024-01-19 15:02:53 +0100

    buttonA${i} " got changed to `

--- logic.js
+++ logic.js
@@ -7,6 +7,6 @@ const buttonContainer = document.getElementById("button-container");
 for (let i = 1; i <= numberOfAdditionButtons; i++) {
   const addButton = document.createElement("button");
   addButton.textContent = "Add 1";
-  addButton.setAttribute("id", "buttonA${i}");
+  addButton.setAttribute("id", `buttonA${i}`);
   buttonContainer.appendChild(addButton);
 }
