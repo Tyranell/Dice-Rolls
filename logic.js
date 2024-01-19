46f8ceb3146f1987d838412864d2231ee8a6051b
Damian Jordan <damianjordan83@gmail.com>
2024-01-19 15:22:37 +0100

    Automatic numbers
    I'm trying to do the same thing I did for the buttons but for the actual numbers themselves.

--- logic.js
+++ logic.js
@@ -1,9 +1,11 @@
 //Spooky! The start of a JS file
 const numberOfAdditionButtons = 20;
 const numberOfSubtractionButtons = 20;
+const numberOfNumbers = 20;
 
 const addButtonContainer = document.getElementById("add-button-container");
 const subButtonContainer = document.getElementById("sub-button-container");
+const numberContainer = document.getElementById("number-container");
 
 for (let i = 1; i <= numberOfAdditionButtons; i++) {
   const addButton = document.createElement("button");
@@ -17,4 +19,11 @@ for (let i = 1; i <= numberOfSubtractionButtons; i++) {
   subButton.textContent = "Subtract 1";
   subButton.setAttribute("id", `buttonS${i}`);
   subButtonContainer.appendChild(subButton);
+}
+
+for (let i = 1; i <= numberOfNumbers; i++) {
+  const numberFields = document.createElement("div");
+  numberFields.textContent = `${1}`;
+  numberFields.setAttribute("id", `number${i}`);
+  numberContainer.appendChild(numberFields);
 }
